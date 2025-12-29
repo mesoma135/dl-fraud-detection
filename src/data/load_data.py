96d2cd6a90bca3c518ec9d665a44e86b73f3799b
mesoma135 <146199540+mesoma135@users.noreply.github.com>
2025-12-29 13:25:32 -0800

    loaded the data

---
+++ src/data/load_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parents[2]
+data_path = base_dir / "data" / "raw" / "creditcard.csv"
+
+if not data_path.exists():
+    raise FileNotFoundError(f"Dataset not found")
+
+if data_path.exists():
+    print("Data loaded successfully")
+
+df = pd.read_csv(data_path)
+
+print(df.shape)
+print(df.columns)
+print(df["Class"].value_counts())
